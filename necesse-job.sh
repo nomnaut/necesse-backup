61ca4dc034291b7c3105e3583fd869efbaacb5a9
nomnaut <finalheaven@gmail.com>
2022-12-03 01:03:45 -0600

    update save script

--- necesse-job.sh
+++ necesse-job.sh
@@ -2,20 +2,23 @@ eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa_linode
 
 saveFiles=""
-for file in ~/necesse-server/saves/*.zip
+for file in /home/necesse/necesse-server/saves/*.zip
 do
     saveFiles="${saveFiles} $file"
 done
 
 for file in "${saveFiles}"
 do
-    currSave="$(md5sum ~/necesse-server/saves/$file)"
-    gittSave="$(md5sum ~/necesse-backup/saves/$file)"
-    if [ $currSave != $gittSave ]; then
-        cp -fr ~/necesse-server/saves/$file ~/necesse-backup/saves/$file
-        git -C ~/necesse-backup/saves/ add -f $file
-        git commit -m "add saves from "$(date +"%m-%d-%Y")
-        git push origin main
-    fi
+    echo $file
+    md5sum /home/necesse/necesse-server/saves/$file
+    md5sum /home/necesse/necesse-backup/saves/$file
+    # currSave="$(md5sum /home/necesse/necesse-server/saves/$file)"
+    # gittSave="$(md5sum /home/necesse/necesse-backup/saves/$file)"
+    # if [ $currSave != $gittSave ]; then
+    #     cp -fr /home/necesse/necesse-server/saves/$file ~/necesse-backup/saves/$file
+    #     git -C /home/necesse/necesse-backup/saves/ add -f $file
+    #     git -C /home/necesse/necesse-backup/saves/ commit -m "add saves from "$(date +"%m-%d-%Y")
+    #     git -C /home/necesse/necesse-backup/saves/ push origin main
+    # fi
 done
 
